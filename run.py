2e542f36941f308d65afc9d2af44376ac733bf38
cmmd13 <carlosmanuelmaldonado@gmail.com>
2021-07-11 15:31:12 -0300

    el flag de cantidad de partidos está en funcionamiento. Se corrigió el run.py

--- run.py
+++ run.py
@@ -5,54 +5,39 @@ import os
 import pandas as pd
 import numpy as np
 
-from matplotlib import pyplot as plt
-from matplotlib.path import Path # Para colores a la gráfica
-from matplotlib.patches import PathPatch # Más para colores
-import seaborn as sns #Libreria donde estan los colores
-
-#TODO: acomodar mip.py, el ex run.py
-#Queremos pasarle 3 paramétros, c, d y si tiene o no que mirar las restricciones de partidos
 from mip.py import minimax
 from seleccionador_de_partidos.py import seleccionar_partidos
 
 def main():
-    mejor_iter = 0
-    mejor_sol = null
-    mejor_optimo_fo = 10000
-    anterior_fue_factible = True
-
-    c = 4
-    d = 14
-
+    mejor_c, mejor_d = 0, 18
+    c, d = 4, 14
 
     # El primer fixture de la ejecucion
-    anterior_fue_factible, mejor_sol, mejor_optimo_fo = minimax(c, d, considerar_partidos=False)
+    # Vamos a asumir q siempre va a considerar el fixture pre armado, y q empieza vacio.
+    anterior_fue_factible, mejor_sol, mejor_optimo_fo = minimax(c, d)
 
     # Iteramos moviendo c y d
     while anterior_fue_factible:
-        c+=1
-        d-=1
-        #TODO: armar seleccionador de partidos
+        c += 1
+        d -= 1
         seleccionar_partidos(args.partidos, c, d)
-        anterior_fue_factible, mejor_sol, mejor_optimo_fo = minimax(c, d, iter, considerar_partidos=True)
+        anterior_fue_factible, sol, optimo_fo = minimax(c, d)
+        if optimo_fo == 0:
+            mejor_sol = sol
+            mejor_optimo_fo = optimo_fo
 
-    #TODO: pensar si no habría q tirar un reporte
+    print(mejor_c, mejor_d, mejor_sol)
+
 
 if __name__=="__main__":
-    #TODO: arreglar flags
     # Creamos flags:
     parser = argparse.ArgumentParser(
-        description='queremos fijar la cantidad de iteraciones que vamos a hacer en e'
-    )
-    parser.add_argument(
-        "--iter",
-        default='mirror',
-        choices=['mirror', 'frances', 'ingles', 'invertido', 'mano_a_mano', 'minimax']
+        description='queremos fijar la cantidad de partidos que vamos a fijar entre iteraciones'
     )
     parser.add_argument(
         "--partidos",
-        default='mirror',
-        choices=['mirror', 'frances', 'ingles', 'invertido', 'mano_a_mano', 'minimax']
+        default=10,
+        type=int
     )
 
     args = parser.parse_args()
